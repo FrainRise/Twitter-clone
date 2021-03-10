3b2d4aae8df8f1c112f9bac85eb8374ea1642ba2
FrainRise <frainrise@gmail.com>
2021-03-10 17:27:04 +0200

    Chapter 4 commit

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -4,29 +4,47 @@
             <h1 class="user-profile__username">
                 {{ user.username }}
             </h1>
-            <div class="user-profile__follower-count" style="margin: 50px 0">
+            <div class="user-profile__admin-badge" v-if="user.isAdmin">
+                Admin
+            </div>
+            <div class="user-profile__follower-count">
                 <strong>Followers: {{ followers }}</strong>
             </div>
         </div>
-        <button class="btn-follow" @click="followUser">
-            Follow
-        </button>
+        <div class="user-profile__twoots-wrapper">
+            <TwootItem 
+                v-for="twoot in user.twoots" 
+                :key="twoot.id" :username="user.username" 
+                :twoot="twoot" 
+                @favourite="toggleFavourite"
+            />
+        </div>
     </div>
 </template>
 
 <script>
+import TwootItem from './TwootItem'
+
 export default {
     name: 'UserProfile',
+    components: {
+        TwootItem
+    },
     data() {
         return {
         followers: 0,
         user: {
-            id: 1,
-            username: 'FrainRisen',
-            firstName: 'Andrew',
-            lastName: 'Kovalenko',
-            email: 'an.kava@gmail.com',
-            isAdmin: true
+                id: 1,
+                username: 'FrainRisen',
+                firstName: 'Andrew',
+                lastName: 'Kovalenko',
+                email: 'an.kava@gmail.com',
+                isAdmin: true,
+                twoots: [
+                    {id: 1, content: 'Twotter is Amazing!'},
+                    {id: 2, content: "Don't forget to subscribe to @FrainRisen"},
+                    {id: 3, content: 'All lives matter!'},
+                ]
             }
         }
     },
@@ -45,6 +63,9 @@ export default {
     methods: {
         followUser() {
             this.followers += 1;
+        },
+        toggleFavourite(id) {
+            console.log(`Favourited tweet #${id}`)
         } 
     },
     mounted() {
@@ -55,5 +76,33 @@ export default {
 </script>
 
 <style>
+.user-profile {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    width: 100%;
+    padding: 50px 5%;
+}
 
+.user-profile__user-panel {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 5px;
+    border: 1px solid #DFE3E8;
+}
+
+.user-profile__admin-badge {
+    background: rebeccapurple;
+    color: #fff;
+    border-radius: 5px;
+    margin-right: auto;
+    padding: 0 10px;
+    font-weight: bold;
+}
+
+h1{
+    margin: 0;
+}
 </style>
